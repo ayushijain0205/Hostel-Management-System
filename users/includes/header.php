d9ac2e01e7f5a5e27bf7c38f753233b1690541f8
Ayushi Jain <ayushijain382@gmail.com>
2022-09-20 03:30:28 +0530

    first commit

---
+++ users/includes/header.php
@@ -0,0 +1,13 @@
+ <header class="header black-bg"  style=" background: black">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <!--logo start-->
+            <a href="index.html" class="logo" ><b>Hostel Greviences System</b></a>
+                
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php" style=" background: grey">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
